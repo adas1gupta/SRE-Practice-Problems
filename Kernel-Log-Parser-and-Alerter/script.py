855716ef6b511dedef45569f944e090c08d27848
adas1gupta <dasgupta1arjun@gmail.com>
2026-04-01 05:19:25 -0400

    added functionality to parse 1 or more lines

--- Kernel-Log-Parser-and-Alerter/script.py
+++ Kernel-Log-Parser-and-Alerter/script.py
@@ -12,6 +12,23 @@ def signal_handler(signum, frame):
 signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 
+def parse_raw_line(line: str) -> dict:
+    if line.startswith(" "):
+        return {"continuation": True, "message": line.strip()}
+
+    halves = line.split(";")
+    left_half = halves[0].split(",")
+    priority, sequence, timestamp, flags = left_half
+
+    return {
+        "priority": int(priority),
+        "sequence": int(sequence),
+        "timestamp": int(timestamp),
+        "flags": flags,
+        "message": halves[1].strip(),
+        "continuation": False
+    }
+
 if __name__ == "__main__":
     fd = os.open("/dev/kmsg", os.O_RDONLY | os.O_NONBLOCK)
 
