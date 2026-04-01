a2571194c08c81e8a0e1110214355e7448d8e24e
adas1gupta <dasgupta1arjun@gmail.com>
2026-04-01 12:09:27 -0400

    added classification function for kernel log messages

--- Kernel-Log-Parser-and-Alerter/script.py
+++ Kernel-Log-Parser-and-Alerter/script.py
@@ -2,8 +2,24 @@ import threading
 import signal
 import os
 import time
+import re
+from enum import Enum
 
 shutdown = threading.Event()
+
+class Category(Enum):
+    NORMAL = 0
+    OOM = 1
+    MACHINE_CHECK_EXCEPTION = 4
+    HARDWARE_ERROR = 5
+    EDAC = 6
+    DISK_ERROR = 7
+    FILE_SYSTEM_SPECIFIC = 8
+    BLOCK_LAYER_FAILURE = 9
+    IO_ERROR = 10
+    NIC_HUNG_QUEUE = 11
+    LINK_STATE_ISSUES = 12
+    CONNTRACK_TABLE_EXHAUSTED = 13
 
 def signal_handler(signum, frame):
     if signum == signal.SIGINT or signum == signal.SIGTERM:
@@ -28,6 +44,32 @@ def parse_raw_line(line: str) -> dict:
         "message": halves[1].strip(),
         "continuation": False
     }
+
+def classification(parsed: dict) -> Category: 
+    message = parsed["message"]
+
+    if re.search(r"oom-killer", message): return Category.OOM
+    if re.search(r"Out of memory", message): return Category.OOM
+    if re.search(r"Killed process", message): return Category.OOM
+
+    # Hardware
+    if re.search(r"MCE", message): return Category.MACHINE_CHECK_EXCEPTION
+    if re.search(r"Hardware Error", message): return Category.HARDWARE_ERROR
+    if re.search(r"EDAC", message): return Category.EDAC
+    if re.search(r"ata|sd", message): return Category.DISK_ERROR
+
+    # Filesystem 
+    if re.search(r"EXT4-fs error|XFS.*error|BTRFS.*error", message): return Category.FILE_SYSTEM_SPECIFIC
+    if re.search(r"Buffer I/O error", message): return Category.BLOCK_LAYER_FAILURE
+    if re.search(r"I/O error, dev", message): return Category.IO_ERROR
+
+    # Network
+    if re.search(r"NETDEV WATCHDOG", message): return Category.NIC_HUNG_QUEUE
+    if re.search(r"Link is Down", message): return Category.LINK_STATE_ISSUES
+    if re.search(r"nf_conntrack: table full", message): return Category.CONNTRACK_TABLE_EXHAUSTED
+
+    return Category.NORMAL
+
 
 if __name__ == "__main__":
     fd = os.open("/dev/kmsg", os.O_RDONLY | os.O_NONBLOCK)
