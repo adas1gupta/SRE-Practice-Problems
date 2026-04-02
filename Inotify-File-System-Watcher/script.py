0d130e3299d82f13b29223fc7b5c8d784f64859b
adas1gupta <dasgupta1arjun@gmail.com>
2026-04-02 15:12:10 -0400

    added CLI and inotify watch directory functionality

---
+++ Inotify-File-System-Watcher/script.py
@@ -0,0 +1,64 @@
+import argparse
+import inotify_simple
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Directory to watch")
+    parser.add_argument("--directory", required=True, help="directory for Inotify to monitor")
+
+    args = parser.parse_args()
+    watched_directory = args.directory
+
+    inotify = inotify_simple.INotify()
+    wd = inotify.add_watch(watched_directory, 
+                        inotify_simple.flags.CREATE | 
+                        inotify_simple.flags.MODIFY | 
+                        inotify_simple.flags.DELETE |
+                        inotify_simple.flags.MOVED_FROM | 
+                        inotify_simple.flags.MOVED_TO | 
+                        inotify_simple.flags.ATTRIB)
+
+    descriptor_table = {
+        wd: watched_directory
+    }
+
+    file_contents = {}
+    pending_moves = {}
+
+    while True: 
+        events = inotify.read()
+
+        for event in events: 
+            if event.mask & inotify_simple.flags.CREATE:
+                print(f"path: {event.path}, event type: {event.event_type}, timestamp: {event.timestamp}")
+
+                if event.mask & inotify_simple.flags.ISDIR:
+                    nd = inotify.add_watch(watched_directory, 
+                        inotify_simple.flags.CREATE | 
+                        inotify_simple.flags.MODIFY | 
+                        inotify_simple.flags.DELETE |
+                        inotify_simple.flags.MOVED_FROM | 
+                        inotify_simple.flags.MOVED_TO | 
+                        inotify_simple.flags.ATTRIB)
+                    
+                else:
+                    with open(event.path, 'r') as f:
+                        file_contents[event.path] = f.read()
+
+            elif event.mask & inotify_simple.flags.DELETE:
+                print(f"path: {event.path}, event type: {event.event_type}, timestamp: {event.timestamp}")
+            
+            elif event.mask & inotify_simple.flags.MODIFY:
+                with open(event.path, 'r') as f:
+                    diff = f.read()
+                    
+                print(f"path: {event.path}, event type: {event.event_type}, timestamp: {event.timestamp}", diff)
+                file_contents[event.path] = diff
+
+            elif event.mask & inotify_simple.flags.MOVED_FROM:
+                print(f"path: {event.path}, event type: {event.event_type}, timestamp: {event.timestamp}")
+            
+            elif event.mask & inotify_simple.flags.MOVED_TO:
+                print(f"path: {event.path}, event type: {event.event_type}, timestamp: {event.timestamp}")
+            
+            elif event.mask & inotify_simple.flags.ATTRIB:
+                print(f"path: {event.path}, event type: {event.event_type}, timestamp: {event.timestamp}")
