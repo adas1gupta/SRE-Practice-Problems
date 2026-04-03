7bd50a817690cb33eb7e3b2c3d96bb3c27f2954d
adas1gupta <dasgupta1arjun@gmail.com>
2026-04-03 19:10:28 -0400

    added functionality to compute disk metrics

--- Disk-IO-Monitor/script.py
+++ Disk-IO-Monitor/script.py
@@ -18,9 +18,41 @@ def parse_diskstats(filepath: str) -> dict:
     
     return device_dict
 
+def compute_disk_metrics(first_snapshot: dict, second_snapshot: dict, elapsed_time: int) -> dict: 
+    metrics = {}
+
+    for device in second_snapshot.keys():
+        if device in first_snapshot:
+            s1 = first_snapshot[device]
+            s2 = second_snapshot[device]
+
+            delta_reads = s2["reads done"] - s1["reads done"]
+            delta_writes = s2["writes done"] - s1["writes done"]
+            delta_sectors_read = s2["sectors read"] - s1["sectors read"]
+            delta_sectors_written = s2["sectors written"] - s1["sectors written"]
+            delta_time_in_io = s2["time in io"] - s1["time in io"]
+            total_ios = delta_reads + delta_writes
+
+            metrics[device] = {
+                "reads_per_sec": delta_reads / elapsed_time,
+                "writes_per_sec": delta_writes / elapsed_time,
+                "read_mb_per_sec": ((delta_sectors_read * 512) / (1024 * 1024)) / elapsed_time,
+                "write_mb_per_sec": ((delta_sectors_written * 512) / (1024 * 1024)) / elapsed_time,
+                "util_pct": (delta_time_in_io / (elapsed_time * 1000)) * 100,
+                "avg_latency_ms": delta_time_in_io / total_ios if total_ios > 0 else 0,
+            }
+
+    return metrics
+
 if __name__ == "__main__":
     diskstats = f"/proc/diskstats"
 
+    t1 = time.perf_counter()
     first_half = parse_diskstats(diskstats)
-    time.sleep(1)
+    time.sleep(2)
     second_half = parse_diskstats(diskstats)
+    t2 = time.perf_counter()
+
+    elapsed = t2 - t1 
+
+    diskstats_delta = compute_disk_metrics(first_half, second_half, elapsed)
