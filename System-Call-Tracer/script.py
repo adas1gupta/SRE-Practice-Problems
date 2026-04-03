5dd6e29dcb372c69c211c4ca3ab4314794f28fed
adas1gupta <dasgupta1arjun@gmail.com>
2026-04-03 10:08:07 -0400

    added loop to monitor system call usage for child binary

--- System-Call-Tracer/script.py
+++ System-Call-Tracer/script.py
@@ -1,4 +1,12 @@
 import ctypes
+import argparse
+import os
+import sys
+import re
+
+PTRACE_TRACEME = 0
+PTRACE_SYSCALL = 24
+PTRACE_GETREGS = 12
 
 class UserRegsStruct(ctypes.Structure):
     _fields_ = [
@@ -36,3 +44,56 @@ if __name__ == "__main__":
     ]
 
     libc.ptrace.restype = ctypes.c_long
+
+    parser = argparse.ArgumentParser(description="Monitor system call usage for any given binary")
+    parser.add_argument("--binary", required=True, help="Run a program and monitor its system call usage")
+    parser.add_argument("--args", nargs='*', default=[], required=False, help="Run target program with its arguments")
+    parsed = parser.parse_args()
+    binary = parsed.binary 
+    binary_args = parsed.args or []
+
+    # numbers to system calls mapping
+    syscall_names = {}
+    with open("/usr/include/x86_64-linux-gnu/asm/unistd_64.h") as f:
+        for line in f:
+            match = re.match(r'#define __NR_(\w+)\s+(\d+)', line)
+            if match:
+                name, number = match.groups()
+                syscall_names[int(number)] = name
+
+    pid = os.fork()
+
+    if pid == 0:
+        libc.ptrace(PTRACE_TRACEME, 0, None, None)
+        os.execvp(binary, [binary] + binary_args)
+    else:
+        _, status = os.waitpid(pid, 0)
+        if not os.WIFSTOPPED(status):
+            print("child didn't stop cleanly")
+            sys.exit(1)
+        
+        libc.ptrace(PTRACE_SYSCALL, pid, None, None)
+
+        entry = True
+        sys_num = None
+                
+        while True: 
+            _, status = os.waitpid(pid, 0)
+            if os.WIFSIGNALED(status):
+                print("child killed")
+                sys.exit(0)
+            if os.WIFEXITED(status):
+                print("child exited")
+                sys.exit(0)
+                
+            registers = UserRegsStruct()
+            libc.ptrace(PTRACE_GETREGS, pid, None, ctypes.byref(registers))
+
+            if entry:
+                sys_num = registers.orig_rax
+            else:
+                return_val = registers.rax 
+                print(f"system call: {syscall_names[sys_num]}, arguments: {registers.rdi, registers.rsi, registers.rdx}, return value: {return_val}")
+
+            entry = not entry
+            libc.ptrace(PTRACE_SYSCALL, pid, None, None)
