cf8aa93f6d35e2c84bbcadbdfb70bda7ec70a2fc
adas1gupta <dasgupta1arjun@gmail.com>
2026-05-05 21:26:18 -0400

    added job submission functionality

--- Batch-Job-Queue-With-Dependencies/script.py
+++ Batch-Job-Queue-With-Dependencies/script.py
@@ -2,6 +2,15 @@ from dataclasses import dataclass, field
 from uuid import UUID, uuid4
 from graphlib import TopologicalSorter
 from concurrent.futures import ThreadPoolExecutor
+from enum import Enum 
+
+class JobState(Enum):
+    PENDING = 1
+    FAILED = 2
+    BLOCKED = 3
+    RUNNING = 4
+    COMPLETED = 5
+
 
 @dataclass
 class BatchJob:
@@ -16,8 +25,17 @@ class BatchJob:
 class Scheduler:
     def __init__(self):
         self.jobs: dict[UUID, BatchJob] = {}
-        self.job_state: dict[UUID, str] = {}
+        self.job_state: dict[UUID, JobState] = {}
         self.retry_counts: dict[UUID, int] = {}
         self.dag: TopologicalSorter | None = None
         self.executor: ThreadPoolExecutor | None = None
         self.execution_log: list[dict] = []
+
+    def submit_job(self, job: BatchJob) -> None:
+        if job.job_id not in self.jobs:
+            self.jobs[job.job_id] = job 
+        else:
+            raise ValueError(f"Job {job.job_id} already submitted")
+        
+        self.job_state[job.job_id] = JobState.PENDING
+        self.retry_counts[job.job_id] = 0
